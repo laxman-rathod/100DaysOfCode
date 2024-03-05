36d4ca590cd8126504f7b8513b6a69cc41490dc1
Laxman Rathod <luckyrathod1137@gmail.com>
2024-03-05 12:50:46 +0530

    LeetCode day1 problem solved

---
+++ LeetCode/Day_1_Easy/leetcode_day1_problem1.js
@@ -0,0 +1,16 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function () {
+  if (this.length > 0) {
+    // Check if the array not empty
+    return this[this.length - 1]; // Return last element
+  } else {
+    return -1; // Return -1 for empty array
+  }
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
